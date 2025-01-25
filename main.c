71f6f62602bf75038e50ff2e7d7ce40387242715
kolesnikkr <152421935+kolesnikkr@users.noreply.github.com>
2025-01-25 20:14:08 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -6,25 +6,25 @@
 
 static void Menu() {
 
-    printf("Операції над матрицями:\n");
-    printf("1.Обернена матриця\n");
-    printf("2.Транспонування матриці\n");
-    printf("3.множення на скаляр\n");
-    printf("4.Додавання матриць\n");
-    printf("5.Віднімання матриць\n");
-    printf("6.Множення матриць\n");
-    printf("7.Визначник\n");
-    printf("0.Завершення роботи\n");
-    printf("Виберіть операцію для виконання:\n");
+    printf("Matrix operations:\n");
+    printf("1.Inverse matrix\n");
+    printf("2.Transpose of a matrix\n");
+    printf("3.Multiplication by a scalar\n");
+    printf("4.Adding matrices\n");
+    printf("5.Subtraction of matrices\n");
+    printf("6.Matrix multiplication\n");
+    printf("7.Determiner\n");
+    printf("0.Completion of work\n");
+    printf("Select the operation to perform.:\n");
 }
 
 static void inputMatrix(int rows, int cols, double matrix[MAX_SIZE][MAX_SIZE]) {
-    printf("Введіть елементи матриці (%d x %d):\n", rows, cols);
+    printf("Enter the matrix elements (%d x %d):\n", rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            printf("Елемент [%d][%d]: ", i + 1, j + 1);
+            printf("Element [%d][%d]: ", i + 1, j + 1);
             while (scanf_s("%lf", &matrix[i][j]) != 1) {
-                printf("Введіть дійсне число: ");
+                printf("Enter a valid number: ");
                 while (getchar() != '\n');
             }
         }
@@ -70,7 +70,7 @@ static double determinant(int n, double matrix[MAX_SIZE][MAX_SIZE]) {
 static int inverseMatrix(int n, double matrix[MAX_SIZE][MAX_SIZE], double result[MAX_SIZE][MAX_SIZE]) {
     double det = determinant(n, matrix);
     if (fabs(det) < 1e-9) {
-        printf("Матриця не має оберненої (визначник = 0).\n");
+        printf("The matrix has no inverse (determinant = 0).\n");
         return 0;
     }
 
@@ -129,7 +129,7 @@ static void subtractMatrices(int rows, int cols, double A[MAX_SIZE][MAX_SIZE], d
 
 static void multiplyMatrices(int rowsA, int colsA, double A[MAX_SIZE][MAX_SIZE], int rowsB, int colsB, double B[MAX_SIZE][MAX_SIZE], double result[MAX_SIZE][MAX_SIZE]) {
     if (colsA != rowsB) {
-        printf("Множення неможливе (кількість стовпців A має дорівнювати кількості рядків B).");
+        printf("Multiplication is not possible (the number of columns of A must equal the number of rows of B).");
         return;
     }
 
@@ -152,115 +152,115 @@ int main() {
         Menu();
 
         if (scanf_s("%d", &choice) != 1 || choice < 0 || choice > 7) {
-            printf(" Введіть число від 0 до 7.\n");
+            printf(" Enter a number from 0 to 7.\n");
             while (getchar() != '\n');
             continue;
         }
 
         switch (choice) {
         case 1:
-            printf("Введіть розмір квадратної матриці (n x n): ");
+            printf("Enter the size of the square matrix (n x n): ");
             while (scanf_s("%d", &rowsA) != 1 || rowsA <= 0 || rowsA > MAX_SIZE) {
-                printf("Введіть ціле число від 1 до %d: ", MAX_SIZE);
+                printf("Enter an integer from 1 to %d: ", MAX_SIZE);
                 while (getchar() != '\n');
             }
             colsA = rowsA;
             inputMatrix(rowsA, colsA, A);
             if (inverseMatrix(rowsA, A, result)) {
-                printf("Обернена матриця:\n");
+                printf("Inverse matrix:\n");
                 printMatrix(rowsA, colsA, result);
             }
             break;
         case 2:
-            printf("Введіть розмір матриці (рядки стовпці): ");
+            printf("Enter the size of the matrix (n x m): ");
             while (scanf_s("%d %d", &rowsA, &colsA) != 2 || rowsA <= 0 || colsA <= 0 || rowsA > MAX_SIZE || colsA > MAX_SIZE) {
-                printf("Введіть два додатних числа: ");
+                printf("Enter two positive numbers: ");
                 while (getchar() != '\n');
             }
             inputMatrix(rowsA, colsA, A);
             transposeMatrix(rowsA, colsA, A, result);
-            printf("Транспонована матриця:\n");
+            printf("Transposed matrix:\n");
             printMatrix(colsA, rowsA, result);
             break;
         case 3:
-            printf("Введіть розмір матриці (рядки стовпці): ");
+            printf("Enter the size of the matrix (n x m): ");
             while (scanf_s("%d %d", &rowsA, &colsA) != 2 || rowsA <= 0 || colsA <= 0 || rowsA > MAX_SIZE || colsA > MAX_SIZE) {
-                printf("Введіть два додатних числа: ");
+                printf("Enter two positive numbers: ");
                 while (getchar() != '\n');
             }
             inputMatrix(rowsA, colsA, A);
             double scalar;
-            printf("Введіть скаляр: ");
+            printf("Enter the scalar: ");
             while (scanf_s("%lf", &scalar) != 1) {
-                printf("Введіть дійсне число: ");
+                printf("Enter a real number: ");
                 while (getchar() != '\n');
             }
             multiplyMatrixByScalar(rowsA, colsA, A, scalar, result);
-            printf("Результат множення на скаляр:\n");
+            printf("The result of multiplication by a scalar:\n");
             printMatrix(rowsA, colsA, result);
             break;
         case 4:
-            printf("Введіть розмір матриць (рядки стовпці): ");
+            printf("Enter the size of the matrix (n x m): ");
             while (scanf_s("%d %d", &rowsA, &colsA) != 2 || rowsA <= 0 || colsA <= 0 || rowsA > MAX_SIZE || colsA > MAX_SIZE) {
-                printf("Введіть два додатних числа: ");
+                printf("Enter two positive numbers: ");
                 while (getchar() != '\n');
             }
             inputMatrix(rowsA, colsA, A);
             inputMatrix(rowsA, colsA, B);
             addMatrices(rowsA, colsA, A, B, result);
-            printf("Результат додавання:\n");
+            printf("Addition result:\n");
             printMatrix(rowsA, colsA, result);
             break;
         case 5:
-            printf("Введіть розмір матриць (рядки стовпці): ");
+            printf("Enter the size of the matrix (n x m): ");
             while (scanf_s("%d %d", &rowsA, &colsA) != 2 || rowsA <= 0 || colsA <= 0 || rowsA > MAX_SIZE || colsA > MAX_SIZE) {
-                printf("Введіть два додатних числа: ");
+                printf("Enter two positive numbers: ");
                 while (getchar() != '\n');
             }
             inputMatrix(rowsA, colsA, A);
             inputMatrix(rowsA, colsA, B);
             subtractMatrices(rowsA, colsA, A, B, result);
-            printf("Результат віднімання:\n");
+            printf("Subtraction result:\n");
             printMatrix(rowsA, colsA, result);
             break;
         case 6:
             
-            printf("Введіть розмір першої матриці (рядки стовпці): ");
+            printf("Enter the size of the matrix (n x m): ");
             while (scanf_s("%d %d", &rowsA, &colsA) != 2 || rowsA <= 0 || colsA <= 0 || rowsA > MAX_SIZE || colsA > MAX_SIZE) {
-                printf("Введіть два додатних числа: ");
+                printf("Enter two positive numbers: ");
                 while (getchar() != '\n');
             }
             inputMatrix(rowsA, colsA, A);
-            printf("Введіть розмір другої матриці (рядки стовпці): ");
+            printf("Enter the size of the matrix (n x m): ");
             while (scanf_s("%d %d", &rowsB, &colsB) != 2 || rowsB <= 0 || colsB <= 0 || rowsB > MAX_SIZE || colsB > MAX_SIZE) {
-                printf("Введіть два додатних числа: ");
+                printf("Enter two positive numbers: ");
                 while (getchar() != '\n');
             }
             if (colsA != rowsB) {
-                printf("Множення неможливе (кількість стовпців першої матриці має дорівнювати кількості рядків другої).\n");
+                printf("Multiplication is not possible (the number of columns of the first matrix must equal the number of rows of the second).\n");
                 break;
             }
             inputMatrix(rowsB, colsB, B);
             multiplyMatrices(rowsA, colsA, A, rowsB, colsB, B, result);
-            printf("Результат множення:\n");
+            printf("The result of multiplication:\n");
             printMatrix(rowsA, colsB, result);
             break;
         case 7:
-            printf("Введіть розмір квадратної матриці (n x n): ");
+            printf("Enter the size of the matrix (n x n): ");
             while (scanf_s("%d", &rowsA) != 1 || rowsA <= 0 || rowsA > MAX_SIZE) {
-                printf("Введіть ціле число від 1 до %d: ", MAX_SIZE);
+                printf("Enter an integer from 1 to %d: ", MAX_SIZE);
                 while (getchar() != '\n');
             }
             colsA = rowsA;
             inputMatrix(rowsA, colsA, A);
             double det = determinant(rowsA, A);
-            printf("Визначник матриці: %.2lf\n", det);
+            printf("Matrix determinant: %.2lf\n", det);
             break;
         case 0:
-            printf("Завершення роботи.\n");
+            printf("Completion of work.\n");
             break;
         default:
-            printf("Невірний вибір.\n");
+            printf("Wrong choice.\n");
         }
     } while (choice != 0);
 
